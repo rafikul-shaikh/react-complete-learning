c36736292fa22c2ad0aff0214f9a022aa19c7fec
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-06 17:07:07 +0530

    Remove old Item.jsx from src

--- 18-Fragments/learning-fragment/src/Item.jsx
+++
@@ -1,8 +0,0 @@
-const Item = ({ foodItem }) => {
-  return (
-    <li className="list-group-item my-item">
-      <span className="my-span">{foodItem}</span>
-    </li>
-  );
-};
-export default Item;
