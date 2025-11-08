1f90b9cb76b80543f261703175dc8fc884e858a4
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-08 16:39:07 +0530

    todo-app-v3 project added

---
+++ Projects/6-todo-app-version-three/src/App.jsx
@@ -0,0 +1,43 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+
+function App() {
+  const initialTodoItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Go to College",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Like this video",
+      dueDate: "right now",
+    },
+  ];
+
+  const [todoItems, setTodoItems] = useState(initialTodoItems);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`new item added : ${itemName} Date: ${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      <TodoItems todoItems={todoItems}></TodoItems>
+    </center>
+  );
+}
+
+export default App;
---
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [dueDate, setDueDate] = useState();
+
+  const hanleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+  const hanleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, dueDate);
+    setTodoName(""); //to blank todo name after typing
+    setDueDate(""); //to blank due date after typing
+  };
+
+  return (
+    <div className="container text-center">
+      <div className="row my-row">
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter your name here "
+            value={todoName}
+            onChange={hanleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" value={dueDate} onChange={hanleDateChange} />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-success  my-button"
+            onClick={handleAddButtonClicked}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
