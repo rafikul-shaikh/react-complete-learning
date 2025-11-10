78a6155965abe779c230ae3df75bb44ae65f104d
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-10 23:26:31 +0530

    some new  file added and some modified

--- Projects/7-mini-social-media/src/App.jsx
+++ Projects/7-mini-social-media/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>placeholder</div>
-    </>
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
   );
 }
 
