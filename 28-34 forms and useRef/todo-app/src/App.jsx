625976cc77e17f6d0cd6b1ca62e0905b88519c0e
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-11 09:49:04 +0530

    form and useRef implemented in todo-app

--- 28-34 forms and useRef/todo-app/src/App.jsx
+++ 28-34 forms and useRef/todo-app/src/App.jsx
@@ -1,26 +1,23 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import "./App.css";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
-    //console.log(`new item added : ${itemName} Date: ${itemDueDate}`);
-    const newTodoItems = [
-      ...todoItems,
+    setTodoItems((currValue) => [
+      ...currValue,
       { name: itemName, dueDate: itemDueDate },
-    ];
-    setTodoItems(newTodoItems);
+    ]);
   };
 
   const handleDeleteItem = (todoItemName) => {
     const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
     setTodoItems(newTodoItems);
-    console.log(`item deleted ${todoItemName}`);
   };
 
   return (
--- 28-34 forms and useRef/todo-app/src/components/AddTodo.jsx
+++ 28-34 forms and useRef/todo-app/src/components/AddTodo.jsx
@@ -1,47 +1,38 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { IoMdAddCircleOutline } from "react-icons/io";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const hanleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const hanleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-
-  const handleAddButtonClicked = () => {
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setTodoName(""); //to blank todo name after typing
-    setDueDate(""); //to blank due date after typing
   };
 
   return (
     <div className="container text-center">
-      <div className="row my-row">
+      <form className="row my-row" onSubmit={handleAddButtonClicked}>
         <div className="col-6">
           <input
             type="text"
-            placeholder="Enter your name here "
-            value={todoName}
-            onChange={hanleNameChange}
+            ref={todoNameElement}
+            placeholder="Enter Todo Here"
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={hanleDateChange} />
+          <input type="date" ref={dueDateElement} />
         </div>
         <div className="col-2">
-          <button
-            type="button"
-            className="btn btn-success  my-button"
-            onClick={handleAddButtonClicked}
-          >
+          <button type="submit" className="btn btn-success my-button">
             <IoMdAddCircleOutline />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
