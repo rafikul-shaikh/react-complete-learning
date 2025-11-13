469a2850c174752a41d50fd17d749d6bbec92592
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-13 12:54:46 +0530

    added LoadingSpinner file and useEffect consept  implemented

--- 37-40 Advance-React/src/components/PostList.jsx
+++ 37-40 Advance-React/src/components/PostList.jsx
@@ -1,26 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+  const [fetching, setFetching] = useState(false); //initially i set false before sending request to server or to fetch api
+
+  useEffect(() => {
+    setFetching(true);
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
-  };
+
+    return () => {
+      console.log("clean up called");
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
