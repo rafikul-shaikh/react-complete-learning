c0e232ba9d09d8ed0041378800e1df2e0d7270b1
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-18 12:18:32 +0530

    added components and file modified

---
+++ 42-React-Redux-Tulkit/2-Counter-React-Redux/src/components/Controls.jsx
@@ -0,0 +1,28 @@
+const Controls = () => {
+  return (
+    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <button type="button" className="btn btn-primary">
+        +1
+      </button>
+      <button type="button" className="btn btn-success">
+        -1
+      </button>
+      {/* <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-light">
+        Light
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button> */}
+    </div>
+  );
+};
+export default Controls;
---
+++ 42-React-Redux-Tulkit/2-Counter-React-Redux/src/components/DisplayCounter.jsx
@@ -0,0 +1,4 @@
+const DisplayCounter = () => {
+  return <p className="lead mb-4">This is the counter current value</p>;
+};
+export default DisplayCounter;
